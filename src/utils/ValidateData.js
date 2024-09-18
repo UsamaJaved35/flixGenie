5f30d07690908aba58cbb34be6a65c41c79d115e
UsamaJaved35 <usamamughal4949@gmail.com>
2024-09-19 00:36:04 +0500

    updated regex and password message

--- src/utils/ValidateData.js
+++ src/utils/ValidateData.js
@@ -1,10 +1,9 @@
 const validateData=(email,password)=>{
    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-   const isPassValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password);
+   const isPassValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
 
    if(!isEmailValid) return "Email is not valid";
-   if(!isPassValid) return "Password is not valid"
-
+   if(!isPassValid) return "Password must be at least 8 characters long, and include uppercase, lowercase, number, and special character."
    return null;
 }
 export default validateData;
